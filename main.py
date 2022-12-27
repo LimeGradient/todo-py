ba3e0b0656561d3d24cb38c16edfe8171e684a02
LimeGradient <kfb13@scasd.org>
2022-12-26 23:22:34 -0500

    FEAT: Commands and Saving Modes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import base64
 
 from colorama import Fore
 
+# Cool looking logo
 print(Fore.LIGHTBLUE_EX, ''' 
 ___________        .___              __________        
 \__    ___/___   __| _/____          \______   \___.__.
@@ -11,9 +12,27 @@ ___________        .___              __________
                     \/                          \/     
 ''')
 
+commands = [
+  ".exit",
+  ".read"
+]
+
 def Todo_Add(todo):
     # Encode message for .todo file
     todo_bytes = todo.encode('ascii')
     todo_base64 = base64.b64encode(todo_bytes)
     todo_message = todo_base64.decode('ascii')
-    print(todo_message)
+    
+print(Fore.WHITE)
+print("How would you like your Todo list to be saved?")
+print("[1] Local Storage")
+print("[2] Online Storage")
+mode = input("Saving Mode: ")
+
+if mode == "1":
+  while True:
+    todo = input("Todo: ")
+    if todo in commands:
+      print(todo)
+    else:
+      Todo_Add(todo)
